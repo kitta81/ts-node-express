f3700ecc1ca38191069360d55ee972f1c16b4545
orbit0801 <orbit0801@gmail.com>
2019-02-21 16:07:10 +0900

    now auto deploy test

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ const app = Express();
 const PORT: Number = 1234;
 
 app.get('/', (req: Express.Request, res: Express.Response) => {
-  const msg: string = 'Hello World x Typescript x Express x now';
+  const msg: string = 'Hello World x Typescript x Express x now (auto deploy)';
   res.send(msg);
 });
 
