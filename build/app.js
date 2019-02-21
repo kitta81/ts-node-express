36cb505e589faa50e3d93697cef42c3dadd48e6f
orbit0801 <orbit0801@gmail.com>
2019-02-21 16:09:41 +0900

    add build.js for Now auto deploy.

---
+++ build/app.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Express = require("express");
+const app = Express();
+const PORT = 1234;
+app.get('/', (req, res) => {
+    const msg = 'Hello World x Typescript x Express';
+    res.send(msg);
+});
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+});
+exports.default = app;
+//# sourceMappingURL=app.js.map
